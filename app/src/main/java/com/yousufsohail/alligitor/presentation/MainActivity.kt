99bf5204091cb3a3dbbeebaf99ed6ce286a20e32
Yousuf Sohail <YousufSohail@users.noreply.github.com>
2022-05-24 16:56:52 +0500

    Add support for toggle dark and light theme

--- app/src/main/java/com/yousufsohail/alligitor/presentation/MainActivity.kt
+++ app/src/main/java/com/yousufsohail/alligitor/presentation/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.yousufsohail.alligitor.presentation
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.yousufsohail.alligitor.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +14,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_day_night_mode -> {
+                // Get new mode.
+                val mode =
+                    if ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
+                        Configuration.UI_MODE_NIGHT_NO
+                    ) {
+                        AppCompatDelegate.MODE_NIGHT_YES
+                    } else {
+                        AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                    }
+
+                // Change UI Mode
+                AppCompatDelegate.setDefaultNightMode(mode)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
